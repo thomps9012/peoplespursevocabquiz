fb1b603421ab309db688b7731c77bdbb6e2f9d37
Samuel Thompson <thomps9012@gmail.com>
2020-10-02 18:28:13 -0400

    added in Front landing page

---
+++ src/components/pages/Front/FrontPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../../Header';
+import Footer from '../../Footer';
+
+function Front() {
+    return(
+        <>
+        <Header />
+        <div className="NonProfitBanner">
+            <h3>The People's Purse is a nonprofit organization dedicated to collecting data to represent how you want to spend your tax dollars.</h3>
+            <p>First we're going to quiz your knowledge on some basic vocabulary for today's lesson</p>
+        </div>
+        <Footer />
+        </>
+    )
+}
+export default Front;
