54df6c6e57d3279bb223bc3e9f869ada7a94c67d
Samuel Thompson <thomps9012@gmail.com>
2020-10-05 14:20:16 -0400

    getting new error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
-import Front from './components/pages/Tax/Tax';
+// import Front from './components/pages/Front/FrontPage';
+import Tax from './components/pages/Tax/Tax';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       
-        <Front />
+        {/* <Front /> */}
+        <Tax />
      
     </div>
   );
--- src/components/pages/Tax/Tax.js
+++ src/components/pages/Tax/Tax.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaxImg from '../../../assets/tax.png';
 import Footer from '../../Footer/Footer';
 import Header from '../../Header/Header';
-import NextQues from '../../NextQues/NextQues';
-import AnswerForm from '../../AnswerForm/AnswerForm';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 import  Card  from 'react-bootstrap/Card';
+import { Button } from 'react-bootstrap';
 
 function Tax() {
+    const [value, setValue] = useState(' ');
+    localStorage.setItem('definition', value);
+    console.log(setValue)
+  
     return(
 <>
 <Header />
@@ -18,8 +23,19 @@ function Tax() {
     <Card.Text>
       We pay our taxes to the federal government to receive services from the government (i.e. healthcare)
     </Card.Text>
-    <AnswerForm />
-    <NextQues />
+    <InputGroup className="mb-3">
+    <InputGroup.Prepend>
+      <InputGroup.Text id="inputGroup-sizing-default">Type Your Definition Here!</InputGroup.Text>
+    </InputGroup.Prepend>
+    <FormControl
+      aria-label="Default"
+      aria-describedby="inputGroup-sizing-default"
+      onChange={setValue}
+    />
+  </InputGroup>
+    <Button variant="warning" size="lg" onClick={() => setValue()}>
+    Next Vocab Word
+      </Button>
   </Card.Body>
 </Card>
 <Footer />
