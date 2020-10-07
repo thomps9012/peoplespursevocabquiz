f19fff101845e34b4d1b94663162c7b04cfb7400
Samuel Thompson <thomps9012@gmail.com>
2020-10-07 09:34:49 -0400

    added in executive component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
-import Front from './components/pages/Front/FrontPage';
+// import Front from './components/pages/Front/FrontPage';
 // import Tax from './components/pages/Tax/Tax';
 // import Fiscal from './components/pages/Fiscal/FiscalYear';
+import Executive from './components/pages/Exec/Executive';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       
-        <Front />
+        {/* <Front /> */}
         {/* <Tax /> */}
         {/* <Fiscal /> */}
+        <Executive />
 
      
     </div>
---
+++ src/components/pages/Exec/Executive.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import ExecutivePic from '../../../assets/executive.png';
+import Footer from '../../Footer/Footer';
+import Header from '../../Header/Header';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import  Card  from 'react-bootstrap/Card';
+import { Button } from 'react-bootstrap';
+import './Executive.css';
+
+function Tax() {
+    const [value, setValue] = useState(' ');
+    useEffect(() => {
+      localStorage.setItem('fiscal year definition', value);
+    });
+      console.log()
+    
+  
+    return(
+<>
+<Header />
+<Card className="text-center">
+  <Card.Header>Question 1</Card.Header>
+  <Card.Body>
+    <Card.Title>Executive Branch</Card.Title>
+    <Card.Img src={ExecutivePic} />
+    <Card.Text>
+      A new president is elected to the executive branch every four years through an election.
+    </Card.Text>
+    <InputGroup className="mb-3">
+    <InputGroup.Prepend>
+      <InputGroup.Text id="inputGroup-sizing-default">Type Your Definition Here!</InputGroup.Text>
+    </InputGroup.Prepend>
+    <FormControl
+      aria-label="Default"
+      aria-describedby="inputGroup-sizing-default"
+      id="def"
+      />
+  </InputGroup>
+    <Button variant="warning" size="lg" onClick={() => setValue(document.getElementById('def').value)}>
+    Next Vocab Word
+      </Button>
+  </Card.Body>
+</Card>
+<Footer />
+</>
+    )
+}
+export default Tax;
